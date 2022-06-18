fd3a34f1b3745ec78bdf3365adba610fc327e2bd
xy <xy1392055037@outlook.com>
2022-06-18 16:07:12 +0800

    提交购物车后端

--- src/main/java/com/ibm/mallproject/controller/CartController.java
+++ src/main/java/com/ibm/mallproject/controller/CartController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin
@@ -19,25 +21,30 @@ public class CartController {
 
 	@RequestMapping("/findCartByUserId")
 	@ResponseBody
-	public List<CartInfo> findCartByUserId(String user_id) {
+	public List<CartInfo> findCartByUserId(@RequestParam String user_id) {
 		return cartService.findCartByUserId(user_id);
 	}
 
 	@RequestMapping("/insertCart")
 	@ResponseBody
-	public Integer insertCart(CartInfo cartInfo) {
-		return cartService.insertCart(cartInfo);
+	public String insertCart(@RequestParam Map<String, String> map) {
+		CartInfo cartInfo = new CartInfo();
+
+
+		return cartService.insertCart(cartInfo)>0 ? "新增成功" : "新增失败";
 	}
 
 	@RequestMapping("/updateCart")
 	@ResponseBody
-	public Integer updateCart(CartInfo cartInfo) {
-		return cartService.updateCart(cartInfo);
+	public String updateCart(@RequestParam Map<String, String> map) {
+		CartInfo cartInfo = new CartInfo();
+
+		return cartService.updateCart(cartInfo)>0 ? "修改成功" : "修改失败";
 	}
 
 	@RequestMapping("/deleteCartById")
 	@ResponseBody
-	public Integer deleteCartById(String cart_id) {
-		return cartService.deleteCartById(cart_id);
+	public String deleteCartById(@RequestParam String cart_id) {
+		return cartService.deleteCartById(cart_id)>0 ? "删除成功" : "删除失败";
 	}
 }
