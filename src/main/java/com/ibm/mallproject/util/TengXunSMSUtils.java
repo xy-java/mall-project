fcdb0fb69fea9c1423af662d92b6b01959075d1e
yjq <yjq@qq.com>
2022-06-20 21:04:03 +0800

    短信验证码注册工具类

---
+++ src/main/java/com/ibm/mallproject/util/TengXunSMSUtils.java
@@ -0,0 +1,94 @@
+package com.ibm.mallproject.util;
+
+
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.sms.v20190711.SmsClient;
+import com.tencentcloudapi.sms.v20190711.models.SendSmsRequest;
+import com.tencentcloudapi.sms.v20190711.models.SendSmsResponse;
+
+
+/**
+ * @ClassName TengXunSMSUtils
+ * @Author yjq
+ * @Date 2022/6/20 20:17
+ * @Version 1.0
+ * @Description
+ */
+public class TengXunSMSUtils {
+    public static final String VALIDATE_CODE = "866988";
+
+    public static  void sendShortMessage(String signName,String templateCode, String phoneNums, String param)  {
+        try {
+            /*
+             * CAM 密钥查询：https://console.cloud.tencent.com/cam/capi
+             */
+            Credential cred = new Credential("AKIDAtOefQwCDyUlmX3KL6g05uTs3zf6HCsI", "C8pGRM2SdYEepmUw96oGTQ7ZlhNq32lE");
+            // 无特殊需求可以跳过
+            HttpProfile httpProfile = new HttpProfile();
+
+
+            httpProfile.setReqMethod("POST");
+
+            httpProfile.setConnTimeout(60);
+
+            /* 指定接入地域域名，默认就近地域接入域名为 sms.tencentcloudapi.com ，也支持指定地域域名访问，例如广州地域的域名为 sms.ap-guangzhou.tencentcloudapi.com */
+            httpProfile.setEndpoint("sms.tencentcloudapi.com");
+            /* 非必要步骤:
+             * 实例化一个客户端配置对象，可以指定超时时间等配置 */
+            ClientProfile clientProfile = new ClientProfile();
+            /* SDK默认用TC3-HMAC-SHA256进行签名
+             * 非必要请不要修改这个字段 */
+            clientProfile.setSignMethod("HmacSHA256");
+            clientProfile.setHttpProfile(httpProfile);
+            /* 实例化 SMS 的 client 对象
+             * 第二个参数是地域信息，可以直接填写字符串 ap-guangzhou，或者引用预设的常量 */
+            SmsClient client = new SmsClient(cred, "ap-guangzhou", clientProfile);
+            /* 实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数
+             * 您可以直接查询 SDK 源码确定接口有哪些属性可以设置
+             * 属性可能是基本类型，也可能引用了另一个数据结构
+             * 推荐使用 IDE 进行开发，可以方便地跳转查阅各个接口和数据结构的文档说明 */
+            SendSmsRequest req = new SendSmsRequest();
+            /* 填充请求参数,这里request对象的成员变量即对应接口的入参
+             * 你可以通过官网接口文档或跳转到request对象的定义处查看请求参数的定义
+             * 基本类型的设置:
+             * 帮助链接：
+             * 短信控制台: https://console.cloud.tencent.com/smsv2
+             * 腾讯云短信小助手: https://cloud.tencent.com/document/product/382/3773#.E6.8A.80.E6.9C.AF.E4.BA.A4.E6.B5.81 */
+
+            /* 短信应用ID: 短信SdkAppId在 [短信控制台] 添加应用后生成的实际SdkAppId，示例如1400006666 */
+            // 应用 ID 可前往 [短信控制台](https://console.cloud.tencent.com/smsv2/app-manage) 查看
+            String SdkAppid = "1400693535";
+            req.setSmsSdkAppid(SdkAppid);
+            /* 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，可登录 [短信控制台] 查看签名信息 */
+            // 签名信息可前往 [国内短信](https://console.cloud.tencent.com/smsv2/csms-sign)
+            //String signName = "在家学习1公众号";
+            req.setSign(signName);
+            /* 国际/港澳台短信 senderid: 国内短信填空，默认未开通，如需开通请联系 [sms helper] */
+            //String senderid = "";
+            //req.setSenderId(senderid);
+            /* 模板 ID: 必须填写已审核通过的模板 ID，可登录 [短信控制台] 查看模板 ID */
+            req.setTemplateID(templateCode);//---------------------------------
+            /* 下发手机号码，采用 e.164 标准，+[国家或地区码][手机号]
+             * 例如+8613711112222， 其中前面有一个+号 ，86为国家码，13711112222为手机号，最多不要超过200个手机号*/
+            String[] phoneNumber = {"+86" + phoneNums + ""};
+            req.setPhoneNumberSet(phoneNumber);
+            /* 模板参数: 若无模板参数，则设置为空*/
+            String[] templateParams = {param};
+            req.setTemplateParamSet(templateParams);//------------------------------------
+            /* 通过 client 对象调用 SendSms 方法发起请求。注意请求方法名与请求对象是对应的
+             * 返回的 res 是一个 SendSmsResponse 类的实例，与请求对象对应 */
+            SendSmsResponse res = client.SendSms(req);
+            // 输出 JSON 格式的字符串回包
+            System.out.println(SendSmsResponse.toJsonString(res));
+            // 可以取出单个值，您可以通过官网接口文档或跳转到 response 对象的定义处查看返回字段的定义
+            System.out.println(res.getRequestId());
+        } catch (TencentCloudSDKException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
